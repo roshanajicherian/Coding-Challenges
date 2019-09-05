1b804216328a0f17f2ea81756eaf15cd0b798ecf
Roshan Aji Cherian <34731430+roshanajicherian@users.noreply.github.com>
2019-09-05 21:15:02 +0530

    Create Sequence Equation.cpp

---
+++ Sequence Equation.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n=0,t=0,y=0,x=0;
+    cin>>n;
+    int A[n];
+    for(int i=1;i<=n;i++)
+    {
+
+        cin>>A[i];
+    }
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=n;j++)
+        {
+            x=i;
+        if(A[j]==x)
+        {
+            t=j;
+        }
+        for(int k=1;k<=n;k++)
+        {
+        if(A[k]==t)
+        y=k;
+        }
+        }
+        cout<<y<<"\n";
+    }
+
+    return 0;
+}
+
