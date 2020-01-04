14c23312cf2c42f2322e48c8c69514ab37c7f204
Roshan Aji Cherian <34731430+roshanajicherian@users.noreply.github.com>
2020-01-04 16:11:27 +0530

    Prevent occurrence of negative numbers

--- CodeChef/January Long 2019/Chef and Dynamo (DYNAMO).cpp
+++ CodeChef/January Long 2019/Chef and Dynamo (DYNAMO).cpp
@@ -1,16 +1,20 @@
 #include <iostream>
 #include <math.h>
+#include <stdio.h>
+#include<stdlib.h>
 using namespace std;
 
 int main()
 {
     int t=0;
-    int n=0,a=0,b=0,c=0,d=0,e=0,cursum=0,sum=0;
+    cin>>t;
+    while(t--)
+    {
+    long long int n=0,a=0,b=0,c=0,d=0,e=0,cursum=0,sum=0,uplim=1,lwlim=1,lwdis=0,updis=0,temps=0,sum2=0;
     cin>>n;
-    int uplim=pow(10,n);//confirm is 100 or 99
-    int lwlim=1;//confirm if 1 or 0
-    int lwdis=0,updis=0,temps=0;
-    //cin>>n;
+    for(int i=0;i<n;i++)
+        uplim*=10;
+    uplim-=1;
     cin>>a;
     lwdis=abs(a-lwlim);
     updis=abs(a-uplim);
@@ -18,26 +22,37 @@ int main()
     {
         temps=(2*uplim)+3;
     }
-    else if(lwdis<updis)
+    else if(lwdis>=updis)
     {
         temps=(3*uplim)+2;
     }
-    cout<<"S : "<<temps<<'\n';
+    cout<<temps<<'\n';
+    fflush(stdout);
     cin>>b;
     cursum=((a+b)+((2*uplim)+1));
+    sum2=temps-(a+b);
     if(cursum<=temps)
         c=uplim;
     else
-        c=1;
-    cout<<"C : "<<c<<'\n';
+        c=abs(uplim-sum2)-1;
+    cout<<c<<'\n';
+    fflush(stdout);
     cin>>d;
     sum=a+b+c+d;
     e=temps-sum;
-    cout<<"E : "<<e;
+    cout<<e<<'\n';
+    fflush(stdout);
     sum=a+b+c+d+e;
-    cout<<"\nFinal Sum : "<<sum<<'\n';
     if(sum==temps)
+        {
         cout<<1<<'\n';
+        fflush(stdout);
+        }
     else
-        cout<<-1<<'\n';
+     {
+         cout<<-1<<'\n';
+         fflush(stdout);
+         exit(0);
+     }
+    }
 }
