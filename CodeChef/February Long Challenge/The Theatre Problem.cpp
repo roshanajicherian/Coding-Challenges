788fa57d6a1af086232a98ef1fd9f775deb0cd18
Roshan Aji Cherian <34731430+roshanajicherian@users.noreply.github.com>
2020-02-12 17:04:30 +0530

    Update The Theatre Problem.cpp

--- CodeChef/February Long Challenge/The Theatre Problem.cpp
+++ CodeChef/February Long Challenge/The Theatre Problem.cpp
@@ -1,46 +1,28 @@
+//COMPETITION: CODECHEF FEBRUARY LONG CHALLENGE 2020
+//PROBLEM: THE THEATRE PROBLEM CODE: THEATRE
+//AUTHOR:ROSHAN AJI CHERIAN DATE: 12-02-2019
 #include <bits/stdc++.h>
 typedef long long int lli;
 using namespace std;
-class res
+int A[4], B[4], C[4], D[4];
+int M[4][4];
+bool visited[4][4];
+void init(int A[], int B[], int C[], int D[])
 {
-public:
-    int pep[101];
-    int ti[101];
-    char a[101];
-    bool took[101];
-    res()
-    {
-        for (int i = 0; i < 101; i++)
-        {
-            took[i] = false;
-            ti[i] = 0;
-            pep[i] = 0;
-        }
-    }
-};
-res P;
-int cou = 0;
-lli A[4], B[4], C[4], D[4];
-int flg = 0, pos1 = 0, pos2 = 0;
-void init(lli A[], lli B[], lli C[], lli D[])
-{
-    for (lli i = 0; i < 4; i++)
+    for (int i = 0; i < 4; i++)
     {
         A[i] = 0;
         B[i] = 0;
         C[i] = 0;
         D[i] = 0;
     }
-    for (int i = 0; i < 101; i++)
-    {
-        P.took[i] = false;
-        P.ti[i] = 0;
-        P.pep[i] = 0;
-    }
+    for (int i = 0; i < 4; i++)
+        for (int j = 0; j < 4; j++)
+            visited[i][j] = false;
 }
-void arange(char m, lli sh)
+
+void arange(char m, int sh)
 {
-    lli a_c = 0, b_c = 0, c_c = 0, d_d = 0;
     if (m == 'A')
     {
         if (sh == 12)
@@ -86,78 +68,30 @@ void arange(char m, lli sh)
             D[3]++;
     }
 }
-void findsort()
-{
-    for (int i = 0; i < cou - 1; i++)
-    {
-        for (int j = 0; j < cou - i - 1; j++)
-        {
-            if (P.pep[j + 1] > P.pep[j])
-            {
-                swap(P.pep[j + 1], P.pep[j]);
-                swap(P.ti[j + 1], P.ti[j]);
-                swap(P.a[j + 1], P.a[j]);
-            }
-        }
-    }
-}
-lli fin = 0;
-void findmax()
-{
-    lli rate[4] = {100, 75, 50, 25};
-    findsort();
-    lli res[4];
-    for (int i = 0; i < 4; i++)
-        res[i] = 0;
-    lli val = 0;
-    int k = 0;
-    for (int i = 0; i < cou; i++)
-    {
-        if (P.took[i] == false)
-        {
-            res[k] = P.pep[i];
-            k++;
-            P.took[i] = true;
-            for (int j = 0; j <= cou; j++)
-            {
-                if (P.ti[i] == P.ti[j] || P.a[i] == P.a[j])
-                    P.took[j] = true;
-            }
-        }
-    }
-
-    for (int i = 0; i < 4; i++)
-    {
-        if (res[i] <= 0)
-            val -= 100;
-        else
-            val += (rate[i] * res[i]);
-    }
-    cout << val << '\n';
-    fin += val;
-}
 int main()
 {
     ios_base::sync_with_stdio(false);
-    lli t = 0, tres = 0;
-
+    int t = 0, tres = 0;
+    int rate[4] = {100, 75, 50, 25};
+    int res[4];
     cin >> t;
+    int fintot = 0;
     while (t--)
     {
-        vector<lli> max1(4, INT_MIN);
-        vector<lli> maxc(4, INT_MIN);
 
         init(A, B, C, D);
-        lli n = 0, sh = 0;
+        for (int i = 0; i < 4; i++)
+            res[i] = 0;
+        int n = 0, sh = 0;
         char m;
         cin >> n;
-        for (lli i = 0; i < n; i++)
+        for (int i = 0; i < n; i++)
         {
             cin >> m;
             cin >> sh;
             arange(m, sh);
         }
-        lli M[4][4];
+
         for (int i = 0; i < 4; i++)
         {
             M[0][i] = A[i];
@@ -165,21 +99,15 @@ int main()
             M[2][i] = C[i];
             M[3][i] = D[i];
         }
-
-        // for(int i=1;i<4;i++)
-        // {
-        //     for(int j=1;j<4;j++)
-        //     {
-        //         if(M[i][j]==M[i-1][j-1] && M[i][j]==M[i-1][j])
-        //         {
-        //             flg=1;
-        //             break;
-        //             pos1=j-1;
-        //             pos2=i-1;
-        //         }
-        //     }
-        // }
-        //FIND OUT ROW WISE MAXIMUM
+        int cr[4], cc[4];
+        int max1[4];
+        for (int i = 0; i < 4; i++)
+        {
+            max1[i] = INT_MIN;
+            cr[i] = 0;
+            cc[i] = 0;
+        }
+        //find out row max
         for (int i = 0; i < 4; i++)
         {
             max1[0] = max(max1[0], A[i]);
@@ -187,126 +115,88 @@ int main()
             max1[2] = max(max1[2], C[i]);
             max1[3] = max(max1[3], D[i]);
         }
-        //WE SHOULD CONSIDER REPETITIONS ALSO
-        int j = 0;
+
+        /*for (int j = 0; j < 4; j++)
+        {
+            if (max1[0] == M[0][j])
+                cr[0]++;
+            if (max1[0] == M[j][3])
+                cc[0]++;
+            if (max1[1] == M[1][j])
+                cr[1]++;
+            if (max1[1] == M[j][3])
+                cc[1]++;
+            if (max1[2] == M[2][j])
+                cr[2]++;
+            if (max1[2] == M[j][3])
+                cc[2]++;
+            if (max1[3] == M[3][j])
+                cr[3]++;
+            if (max1[3] == M[j][3])
+                cc[3]++;
+        }*/
         for (int i = 0; i < 4; i++)
         {
-            if (A[i] == max1[0] && A[i] != 0)
-            {
-                P.pep[j] = A[i];
-                P.ti[j] = i;
-                P.a[j] = 'A';
-                j++;
-            }
-            if (B[i] == max1[1] && B[i] != 0)
-            {
-                P.pep[j] = B[i];
-                P.ti[j] = i;
-                P.a[j] = 'B';
-                j++;
-            }
-            if (C[i] == max1[2] && C[i] != 0)
-            {
-                P.pep[j] = C[i];
-                P.ti[j] = i;
-                P.a[j] = 'C';
-                j++;
-            }
-            if (D[i] == max1[3] && D[i] != 0)
-            {
-                P.pep[j] = D[i];
-                P.ti[j] = i;
-                P.a[j] = 'D';
-                j++;
-            }
+            if (max1[0] == M[0][i])
+                cr[0]++;
+            if (max1[1] == M[1][i])
+                cr[1]++;
+            if (max1[2] == M[2][i])
+                cr[2]++;
+            if (max1[3] == M[3][i])
+                cr[3]++;
         }
-        vector<char> loc;
-        //FIND OUT COLUMN WISE MAXIMUM
+        int flag[4] = {0, 0, 0, 0};
         for (int i = 0; i < 4; i++)
         {
-            if (A[i] >= B[i] && A[i] >= C[i] && A[i] >= D[i])
-            {
-                maxc[i] = A[i];
-                loc.push_back('A');
-            }
-            if (B[i] >= A[i] && B[i] >= C[i] && B[i] >= D[i])
-            {
-                maxc[i] = B[i];
-                loc.push_back('B');
-            }
-            if (C[i] >= A[i] && C[i] >= B[i] && C[i] >= D[i])
-            {
-                maxc[i] = C[i];
-                loc.push_back('C');
-            }
-            if (D[i] >= A[i] && D[i] >= B[i] && D[i] >= C[i])
-            {
-                maxc[i] = D[i];
-                loc.push_back('D');
-            }
+            if (cr[i] < cc[i] && cr[i] >= 1)
+                flag[i] = 1;
         }
+        int maxr = INT_MIN, loci = 0, locj = 0, l = 0;
         for (int i = 0; i < 4; i++)
         {
-            if (M[i][0] == maxc[0] && M[i][0] != 0)
+            maxr = INT_MIN;
+            for (int j = 0; j < 4; j++)
             {
-                P.pep[j] = M[i][0];
-                P.ti[j] = 0;
-                if (i == 0)
-                    P.a[j] = 'A';
-                else if (i == 1)
-                    P.a[j] = 'B';
-                else if (i == 2)
-                    P.a[j] = 'C';
-                else if (i == 3)
-                    P.a[j] = 'D';
-                j++;
+                if (flag[i] == 0)
+                {
+
+                    if (M[i][j] >= maxr && visited[i][j] == false)
+                    {
+                        maxr = M[i][j];
+                        loci = i;
+                        locj = j;
+                    }
+                }
+                if (flag[i] == 1)
+                {
+                    if (M[i][j] > maxr && visited[i][j] == false)
+                    {
+                        maxr = M[i][j];
+                        loci = i;
+                        locj = j;
+                    }
+                }
             }
-            if (M[i][1] == maxc[1] && M[i][1] != 0)
+            res[l] = maxr;
+            l++;
+            for (int k = 0; k < 4; k++)
             {
-                P.pep[j] = M[i][1];
-                P.ti[j] = 1;
-                if (i == 0)
-                    P.a[j] = 'A';
-                else if (i == 1)
-                    P.a[j] = 'B';
-                else if (i == 2)
-                    P.a[j] = 'C';
-                else if (i == 3)
-                    P.a[j] = 'D';
-                j++;
-            }
-            if (M[i][2] == maxc[2] && M[i][2] != 0)
-            {
-                P.pep[j] = M[i][2];
-                P.ti[j] = 2;
-                if (i == 0)
-                    P.a[j] = 'A';
-                else if (i == 1)
-                    P.a[j] = 'B';
-                else if (i == 2)
-                    P.a[j] = 'C';
-                else if (i == 3)
-                    P.a[j] = 'D';
-                j++;
-            }
-            if (M[i][3] == maxc[3] && M[i][3] != 0)
-            {
-                P.pep[j] = M[i][3];
-                P.ti[j] = 3;
-                if (i == 0)
-                    P.a[j] = 'A';
-                else if (i == 1)
-                    P.a[j] = 'B';
-                else if (i == 2)
-                    P.a[j] = 'C';
-                else if (i == 3)
-                    P.a[j] = 'D';
-                j++;
+                visited[loci][k] = true;
+                visited[k][locj] = true;
             }
         }
-        cou = j;
-        findmax();
+        int tot = 0;
+        sort(res, res + 4, greater<int>());
+        for (int i = 0; i < 4; i++)
+        {
+            if (res[i] == 0)
+                tot -= 100;
+            else
+                tot += (res[i] * rate[i]);
+        }
+        fintot += tot;
+        cout << tot << '\n';
     }
-    cout << fin << '\n';
-    return 0;
+    cout << fintot << '\n';
 }
