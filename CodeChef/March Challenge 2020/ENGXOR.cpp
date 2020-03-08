c8362593d6f936f4a5c32696ca740f16008e57d3
Roshan Aji Cherian <34731430+roshanajicherian@users.noreply.github.com>
2020-03-08 12:00:24 +0530

    Update ENGXOR.cpp

--- CodeChef/March Challenge 2020/ENGXOR.cpp
+++ CodeChef/March Challenge 2020/ENGXOR.cpp
@@ -1,39 +1,60 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long int lli;
-/*lli countsetbits(lli a)
+int lookup[256];
+void scanner(int &n)
 {
-    lli count = 0;
-    while (a)
+    bool isneg = false;
+    register int c;
+    n = 0;
+    c = getchar();
+    if (c == '-')
     {
-        count += a & 1;
-        a >>= 1;
+        isneg = true;
+        c = getchar();
     }
-    return count;
-}*/
+    for (; (c > 47 && c < 58); c = getchar())
+        n = (n << 1) + (n << 3) + c - 48;
+    if (isneg)
+        n *= -1;
+}
+void intial()
+{
+    lookup[0] = 0;
+    for (int i = 1; i < 256; i++)
+        lookup[i] = (i & 1) + lookup[i / 2];
+}
+int countbit(int n)
+{
+    return (lookup[n & 0xff] +
+            lookup[(n >> 8) & 0xff] +
+            lookup[(n >> 16) & 0xff] +
+            lookup[n >> 24]);
+}
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    lli t = 0;
-    cin >> t;
+    int t = 0;
+    scanner(t);
+    intial();
     while (t--)
     {
-        lli n = 0, q = 0;
-        cin >> n >> q;
-        lli A[n];
-        lli res = 0;
-        for (lli i = 0; i < n; i++)
-            cin >> A[i];
+        int n = 0, q = 0;
+        scanner(n);
+        scanner(q);
+        int A[n];
+        int res = 0;
+        for (int i = 0; i < n; i++)
+            scanner(A[i]);
         while (q--)
         {
-            lli qy = 0, evn = 0, odd = 0;
-            cin >> qy;
-            for (lli i = 0; i < n; i++)
+            int qy = 0, evn = 0, odd = 0;
+            scanner(qy);
+            for (int i = 0; i < n; i++)
             {
                 res = qy ^ A[i];
-                if (__builtin_parity(res)==0)
-
+                if (countbit(res) % 2 == 0)
                     evn++;
                 else
                     odd++;
